bc44922a8ece3e3f820ddbacb6108c354b1791b9
“fubo” <fubo@longersec.com>
2022-08-03 22:00:24 +0800

    首页更改

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -2,7 +2,7 @@
   <div class="navbar">
     <hamburger :is-active="sidebar.opened" class="hamburger-container" @toggleClick="toggleSideBar" />
     <div class="app-breadcrumb">
-      安徽博客教育科技股份有限公司
+      久安世纪管理系统
       <span class="breadBtn">体验版</span>
     </div>
     <!-- <breadcrumb class="breadcrumb-container" /> -->
